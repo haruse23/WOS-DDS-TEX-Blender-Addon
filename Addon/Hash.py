4fc9924fd326697173f78ed46477499fdd5b22cc
haruse23 <mrjokeromar123@outlook.com>
2025-12-28 11:09:31 +0200

    Delete Addon directory

--- Addon/Hash.py
+++
@@ -1,7 +0,0 @@
-def Hash(name: str) -> int:
-    hash_value = 0
-    for c in name:
-        if 'A' <= c <= 'Z':
-            c = chr(ord(c) + 0x20)  # lowercase
-        hash_value = (hash_value * 33 + ord(c)) & 0xFFFFFFFF
-    return hash_value
